edfc30a5a066c181e7b8776e61ea353ccfc4a789
Ghada-Elkady <ghada44446666@gmail.com>
2025-03-05 17:32:48 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');  
+const dotenv = require('dotenv');  
+const morgan = require('morgan');  
+dotenv.config({ path: 'config.env' });  
+
+const app = express();  
+
+if (process.env.NODE_ENV === 'development') {  
+    app.use(morgan('dev'));  
+    console.log(`mode: ${process.env.NODE_ENV}`);  
+}  
+
+app.get('/', (req, res) => {  
+    res.send('Our API');  
+});  
+
+const PORT = process.env.PORT || 8000;  
+app.listen(PORT, () => {  
+    console.log(`App running running on port ${PORT}`);  
+});  
+
